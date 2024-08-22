f11208a379ab2387149c930a80373413e7a2c263
krrakshit <rkrkr365@gmail.com>
2024-08-22 15:03:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const qrinput = document.getElementById('qr-input')
+const qrimg = document.getElementById('qr-img')
+const qrbutton = document.getElementById('qr-button')
+
+qrbutton.addEventListener('click',()=>{
+    const inputvalue = qrinput.value
+    console.log(inputvalue)
+
+    if(!inputvalue){
+        alert('Please Enter a valid URL')
+    }
+    else{
+        qrimg.src = `https://api.server.com/v1/create-qr-code/?size=150x150&data=${inputvalue}`
+        qrimg.alt = `QR code for${inputvalue}`
+    }
+})
